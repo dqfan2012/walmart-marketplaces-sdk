cd74318e1f91ff430a76a65386b4e38f50746df5
Samuel Stidham <dqfan2012@gmail.com>
2018-11-15 11:26:10 -0500

    Made the API headers local to the UploadFile function so that the baseApiHeaders don't get changed. This allows multiple different API calls using the same client object.

--- src/Items/BulkUpload.php
+++ src/Items/BulkUpload.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace WalmartMarketplaceApi\Items;
 
-use GuzzleHttp\ClientInterface;
 use WalmartMarketplaceApi\Client\Core;
 use WalmartMarketplaceApi\Interfaces\ItemApiEndpointInterface;
 
@@ -15,19 +14,20 @@ class BulkUpload extends Core implements ItemApiEndpointInterface
     public function UploadFile(string $file, string $accessToken)
     {
         // Finish building the Auth headers for this request.
-        $this->baseApiHeaders['headers']['Content-Type'] = 'multipart/formdata';
-        $this->baseApiHeaders['headers']['Accept'] = 'application/xml';
-        $this->baseApiHeaders['headers']['WM_SEC.ACCESS_TOKEN'] = $accessToken;
+        $apiHeaders = $this->baseApiHeaders;
+        $apiHeaders['headers']['Content-Type'] = 'multipart/formdata';
+        $apiHeaders['headers']['Accept'] = 'application/xml';
+        $apiHeaders['headers']['WM_SEC.ACCESS_TOKEN'] = $accessToken;
 
         // Set the query string params
-        $this->baseApiHeaders['query'] = [
+        $apiHeaders['query'] = [
             'feedType' => 'item'
         ];
 
         // Attach the XML file
-        $this->baseApiHeaders['body'] = file_get_contents($file);
+        $apiHeaders['body'] = file_get_contents($file);
 
-        $response = $this->httpClient->request('POST', self::API_URL, $this->baseApiHeaders);
+        $response = $this->httpClient->request('POST', self::API_URL, $apiHeaders);
 
         $statusCode = $response->getStatusCode();
 
